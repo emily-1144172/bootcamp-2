6ba954b5b6770fe3f129a040074f1c14fb65356a
emily-1144172 <evdberg05@outlook.com>
2026-09-15 14:34:02 +0200

    Opdracht 2 verwerkt

--- main.py
+++ main.py
@@ -20,8 +20,49 @@ for student in studenten:
 
 # Opdracht 2
 # Functie die de gemiddelden berekent
+def average(values: dict[str, float]):
+    grade = 0
+    for value in values:
+        grade += values[value]
+    average_grade = grade / len(values)
+    return average_grade
+
 
 # Variabele maken voor iedere klas
-# Geef deze variabele de waarde van het gemiddelde van de klas (gebruik functie)
+studenten_eerste_klas = {
+ "Anna": 7.5,
+ "Funda": 6.9,
+ "Chen": 8.2,
+}
+studenten_tweede_klas = {
+ "Mohammed": 6.9,
+ "Karin": 7.2,
+ "Thomas": 8.2,
+}
+studenten_derde_klas = {
+ "Lisa": 7.3,
+ "Connor": 4.9,
+ "Pete": 8.9,
+}
+
+# Geef deze variabelen de waarden van het gemiddelde van die klas (gebruik functie)
+average_eerste_klas = average(studenten_eerste_klas)
+average_tweede_klas = average(studenten_tweede_klas)
+average_derde_klas = average(studenten_derde_klas)
+
+# Toon deze in de terminal
+print(average_eerste_klas)
+print(average_tweede_klas)
+print(average_derde_klas)
+
+# Sla de gemiddelden op in een nieuwe dictionary
+averages_dict = {
+    "Klas 1": average_eerste_klas,
+    "Klas 2": average_tweede_klas,
+    "Klas 3": average_derde_klas
+}
+
 # Toon het gemiddelde van alle klassen
 # Toon deze in de terminal
+print(average(averages_dict))
+
